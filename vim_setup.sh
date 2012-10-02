6b1e7c50da1ef92c4d957a34b2ddd5d667aec634
Finbarr O'Callaghan <finbarr.ocallaghan@gmail.com>
2012-10-03 00:16:06 +0100

    updated vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,5 +4,7 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/colors
 
+ln -s ~/.dotfiles/vim/colors/candy.vim ~/.vim/colors
 ln -s ~/.dotfiles/vimrc ~/.vimrc
